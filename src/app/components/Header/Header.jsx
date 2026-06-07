f4c3db88cdb79d431306fa58d616dcf6593131c3
Arthur Gehlen <1arthurgehlen@gmail.com>
2026-06-07 00:26:55 -0300

    feat/Adicionando o restante das cores, adicionando o toast, ajustando a fonte e criando o componente de Header

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+// Utils
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return <header className={styles.header_wrapper}>Oi</header>;
+};
+
+export default Header;
---
+++ src/app/proxy.js
@@ -0,0 +1,4 @@
+// sistema do next para lidar com proteção de rotas
+function proxy() {
+  console.log("Oi :)");
+}
