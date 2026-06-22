785f391f25b1bfac3d70ecde99ed45509bab1e46
Arthur Gehlen <1arthurgehlen@gmail.com>
2026-06-21 22:08:43 -0300

    feat/Finalizando a estrutura das páginas de autenticação (login/signup)

--- src/app/(auth)/cadastrar-se/page.jsx
+++ src/app/(auth)/cadastrar-se/page.jsx
@@ -4,6 +4,11 @@ import styles from "../layout.module.css";
 
 // Components
 import { toast } from "sonner";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// Images
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 // Hooks
 import { createClient } from "@/_lib/supabase/client";
@@ -20,12 +25,15 @@ const page = () => {
   };
 
   return (
-    <form>
-      <h1>Cadastrar-se</h1>
+    <form className={styles.auth_form}>
+      <h1 className={styles.auth_form_title}>Cadastrar-se</h1>
 
       <section className={styles.input_wrapper}>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email" className={styles.auth_label}>
+          Email
+        </label>
         <input
+          className={styles.auth_input}
           type="email"
           name="email"
           id="email"
@@ -34,8 +42,11 @@ const page = () => {
       </section>
 
       <section className={styles.input_wrapper}>
-        <label htmlFor="username">Nome de Usuário</label>
+        <label htmlFor="username" className={styles.auth_label}>
+          Nome de Usuário
+        </label>
         <input
+          className={styles.auth_input}
           type="text"
           name="username"
           id="username"
@@ -45,8 +56,11 @@ const page = () => {
       </section>
 
       <section className={styles.input_wrapper}>
-        <label htmlFor="password">Senha</label>
+        <label htmlFor="password" className={styles.auth_label}>
+          Senha
+        </label>
         <input
+          className={styles.auth_input}
           type="password"
           name="password"
           id="password"
@@ -55,8 +69,11 @@ const page = () => {
       </section>
 
       <section className={styles.input_wrapper}>
-        <label htmlFor="confirm_password">Confirmar Senha</label>
+        <label htmlFor="confirm_password" className={styles.auth_label}>
+          Confirmar Senha
+        </label>
         <input
+          className={styles.auth_input}
           type="password"
           name="confirm_password"
           id="confirm_password"
@@ -65,7 +82,43 @@ const page = () => {
       </section>
 
       <section className={styles.user_options}>
-        <section className={styles}></section>
+        <section className={styles.remember_user_container}>
+          <input
+            className={styles.remember_user_input}
+            type="checkbox"
+            name="remember_user"
+            id="remember_user"
+          />
+          <label className={styles.remember_user_label} htmlFor="remember_user">
+            Lembre-me
+          </label>
+        </section>
+
+        <Link className={styles.account_support_link} href="/recuperar-senha">
+          Esqueceu a senha?
+        </Link>
+      </section>
+
+      <button className={styles.auth_button} type="submit">
+        Criar Conta
+      </button>
+
+      <section className={styles.auth_options_header}>
+        {/* transformar o divider em componente? */}
+        <div className={styles.divider}></div>
+        <p className={styles.divider_text}>Ou</p>
+        <div className={styles.divider}></div>
+      </section>
+
+      <section className={styles.auth_options_container}>
+        <button className={styles.auth_option}>
+          <FontAwesomeIcon icon={faGoogle} size="lg" />
+          Criar conta com o Google
+        </button>
+
+        <Link className={styles.account_support_link} href="/login">
+          Já possui uma conta? Faça Login
+        </Link>
       </section>
     </form>
   );
--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,99 @@
-import React from "react";
+"use client";
+// Hooks
+import { createClient } from "@/_lib/supabase/client";
+import { useState } from "react";
+
+// Utils
+import styles from "../layout.module.css";
+
+// Images
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faArrowRightToBracket, faR } from "@fortawesome/free-solid-svg-icons";
+
+// Components
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const page = () => {
-  return <div>page</div>;
+  const supabase = createClient();
+  const [errors, setErrors] = useState({});
+
+  const validate_inputs = () => {};
+
+  const handle_submit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className={styles.auth_form}>
+      <h1 className={styles.auth_form_title}>Login</h1>
+
+      <section className={styles.input_wrapper}>
+        <label htmlFor="email" className={styles.auth_label}>
+          Email
+        </label>
+        <input
+          className={styles.auth_input}
+          type="email"
+          name="email"
+          id="email"
+        />
+      </section>
+
+      <section className={styles.input_wrapper}>
+        <label htmlFor="password" className={styles.auth_label}>
+          Senha
+        </label>
+        <input
+          className={styles.auth_input}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </section>
+
+      <section className={styles.user_options}>
+        <section className={styles.remember_user_container}>
+          <input
+            className={styles.remember_user_input}
+            type="checkbox"
+            name="remember_user"
+            id="remember_user"
+          />
+          <label className={styles.remember_user_label} htmlFor="remember_user">
+            Lembre-me
+          </label>
+        </section>
+
+        <Link className={styles.account_support_link} href="/recuperar-senha">
+          Esqueceu a senha?
+        </Link>
+      </section>
+
+      <button className={styles.auth_button} type="submit">
+        <FontAwesomeIcon icon={faArrowRightToBracket} />
+        Entrar
+      </button>
+
+      <section className={styles.auth_options_header}>
+        {/* transformar o divider em componente? */}
+        <div className={styles.divider}></div>
+        <p className={styles.divider_text}>Ou</p>
+        <div className={styles.divider}></div>
+      </section>
+
+      <section className={styles.auth_options_container}>
+        <button className={styles.auth_option}>
+          <FontAwesomeIcon icon={faGoogle} />
+          Criar conta com o Google
+        </button>
+
+        <Link className={styles.account_support_link} href="/cadastrar-se">
+          Ainda não possui uma conta? Crie uma
+        </Link>
+      </section>
+    </form>
+  );
 };
 
 export default page;
